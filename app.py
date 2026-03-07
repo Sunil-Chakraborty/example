c884ae01d290e25d558bb7b1ea7a732300eb4c1c
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2026-03-07 18:26:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/report")
-def report():
-    return "<h2>Sample Report Page</h2><p>This is your reporting system.</p>"
-
 if __name__ == "__main__":
     app.run()
+    
