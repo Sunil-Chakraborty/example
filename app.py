576d41d49c75de3aaa91bbea2816377224806fb1
Sunil-Chakraborty <92195193+Sunil-Chakraborty@users.noreply.github.com>
2026-03-07 18:32:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Hello! Your Render app is working."
 
 if __name__ == "__main__":
     app.run()
